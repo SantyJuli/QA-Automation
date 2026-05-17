6b40e4ec9a215d8f3f106e8ecd9bb26c0c8c1408
Julia Anisimova <santy.anisimova@gmail.com>
2026-05-17 12:09:53 +0200

    added Bonus challenges tests

--- tests/saucedemo-products.spec.ts
+++ tests/saucedemo-products.spec.ts
@@ -171,4 +171,34 @@ test.describe('Sauce Demo products', () => {
 
   });
 
+  test('change sorting from low to high', async ({ page }) => {
+    await page.goto('/');
+    await page.locator('[data-test="username"]').fill('standard_user');
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+    await page.locator('[data-test="login-button"]').click();
+
+    await page.locator('[data-test="product-sort-container"]').selectOption('lohi');
+
+    await expect(
+      page.locator('[data-test="inventory-item-name"]').first(),
+    ).toHaveText('Sauce Labs Onesie');
+
+  });
+
+  test('add product and refresh the page', async ({ page }) => {
+    await page.goto('/');
+    await page.locator('[data-test="username"]').fill('standard_user');
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+    await page.locator('[data-test="login-button"]').click();
+
+    await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+
+    await page.reload();
+
+    await expect(
+      page.locator(".shopping_cart_badge"),
+      "Cart badge should show 1 after adding a product"
+    ).toHaveText("1");
+  });
+
 });
