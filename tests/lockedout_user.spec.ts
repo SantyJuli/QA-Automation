22c89361892865c7da06d4541347d3ccea1b7787
Julia Anisimova <santy.anisimova@gmail.com>
2026-05-19 13:08:41 +0200

    Add locked user login validation test

---
+++ tests/lockedout_user.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+import { login } from '../helpers/login';
+import { lockedOutUser } from '../test-data/users';
+
+test("error message on locked out user", async ({ page }) => {
+  await login(page, lockedOutUser);
+  await expect(page.getByTestId("error")).toHaveText(
+    "Epic sadface: Sorry, this user has been locked out." 
+  );
+});
