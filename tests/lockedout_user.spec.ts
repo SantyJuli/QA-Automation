6c0ea475466b6e10d51a00bef46028b442743d54
Julia Anisimova <santy.anisimova@gmail.com>
2026-05-19 15:12:43 +0200

    added test.only

--- tests/lockedout_user.spec.ts
+++ tests/lockedout_user.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 import { login } from '../helpers/login';
 import { lockedOutUser } from '../test-data/users';
 
-test("error message on locked out user", async ({ page }) => {
+test.only("error message on locked out user", async ({ page }) => {
   await login(page, lockedOutUser);
   await expect(page.getByTestId("error")).toHaveText(
     "Epic sadface: Sorry, this user has been locked out." 
